6849956f4556d0a9d714088c1c82cdd4e5545831
meyerdav <meyerdav@student.ethz.ch>
2023-07-03 16:20:48 +0200

    added lime analysis

--- src/lime_reason.py
+++ src/lime_reason.py
@@ -56,7 +56,7 @@ def get_data(vectorize: bool = True):
 
         return sentences, classifications
 
-def lime_analysis(samples,labels, classifier, sample_id=0):
+def lime_analysis(samples,labels, classifier,name, sample_id=1):
     explainer = LimeTextExplainer(class_names=["Normal", "Hate Speech"])
     exp = explainer.explain_instance(samples[sample_id], classifier.predict_proba, num_features=6, labels=[0, 1])
     print(f"Sentence: {samples[sample_id]}")
@@ -68,51 +68,21 @@ def lime_analysis(samples,labels, classifier, sample_id=0):
     print ('Explanation for class Hateful')
     print ('\n'.join(map(str, exp.as_list(label=1))))
     html_object = exp.as_html(labels=[0,1],predict_proba=True,show_predicted_value=True)
-    with open("output/lime.html", "w") as file:
+    with open(f"output/lime_{name}.html", "w") as file:
         file.write(str(html_object))
 
 
 
-def analyze_svm(X_train, X_test, y_train, y_test, vectorizer, classifier: SVC) -> None:
-    print("Current Classifier: SVC")
+def analyze(X_train, X_test, y_train, y_test, vectorizer, classifier: SVC,name) -> None:
+    print(f"Current Classifier: {name}")
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
     print("Accuracy:", accuracy_score(y_test, y_pred))
     print("F1:", f1_score(y_test, y_pred))
     print("Precision:", precision_score(y_test, y_pred))
     print("Recall:", recall_score(y_test, y_pred))
-    lime_analysis(X_test,y_test, classifier)
+    lime_analysis(X_test,y_test, classifier,name)
 
-def analyze_classifier(X_train, X_test, y_train, y_test, vectorizer, classifier) -> None:
-    print("Current Classifier: ", type(classifier).__name__)
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_test)
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print("F1:", f1_score(y_test, y_pred))
-    print("Precision:", precision_score(y_test, y_pred))
-    print("Recall:", recall_score(y_test, y_pred))
-
-    vocabulary_list = [word for word, _ in sorted(vectorizer.vocabulary_.items(), key=lambda x: x[1])]
-
-    feature_importances = list(zip(vocabulary_list, classifier.feature_importances_))
-    sorted_features = sorted(feature_importances, key=lambda x: x[1], reverse=True)
-
-    print("Top words with the most influence (Decision Tree):")
-    for i in range(10):
-        print("Word: '", sorted_features[i][0], "'. Importance:", sorted_features[i][1])
-
-    words = [item[0] for item in sorted_features[:10]]
-    importance = [item[1] for item in sorted_features[:10]]
-
-    fig, ax = plt.subplots()
-    ax.bar(words, importance)
-    ax.set_ylabel('Importance')
-    ax.set_xlabel('Word')
-    ax.set_title(f'Top 10 words with the most influence {type(classifier).__name__}')
-    plt.xticks(rotation=45)
-    plt.savefig(f'{type(classifier).__name__}.png')
-    
-    print("---------------------------------------------")
 
 def main() -> None:
     NUM_SAMPLES = 1000
@@ -121,9 +91,9 @@ def main() -> None:
     classifications = classifications[:NUM_SAMPLES]
     X_train, X_test, y_train, y_test = train_test_split(sentences, classifications, test_size=0.2, random_state=42)
 
-    analyze_svm(X_train, X_test, y_train, y_test, vectorizer, make_pipeline(vectorizer,SVC(random_state=42,kernel="sigmoid",C=1,probability=True)))
-    #analyze_classifier(X_train, X_test, y_train, y_test, vectorizer, DecisionTreeClassifier())
-    #analyze_classifier(X_train, X_test, y_train, y_test, vectorizer, RandomForestClassifier())
+    analyze(X_train, X_test, y_train, y_test, vectorizer, make_pipeline(vectorizer,SVC(random_state=42,kernel="sigmoid",C=1,probability=True)),name="SVC")
+    analyze(X_train, X_test, y_train, y_test, vectorizer, make_pipeline(vectorizer,DecisionTreeClassifier()),name="Decision_tree")
+    analyze(X_train, X_test, y_train, y_test, vectorizer, make_pipeline(vectorizer,RandomForestClassifier()),name= "Random Forest")
 
 if __name__ == "__main__":
     main()
